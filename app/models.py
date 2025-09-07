ac6ab5cd2162db6e2f1f1dfcbe107e95b2d8411e
Beatrice Giudici <bea.giudici96@gmail.com>
2025-09-07 14:28:23 +0200

    Structuring the files and start the Book class

---
+++ app/models.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel, field_validator, Field
+from enum import Enum
+from typing import Optional
+
+class Status(str, Enum):
+    read = 'read'
+    toBeRead = 'to-be-read'
+    reading = 'reading'
+
+# Create book
+class BookCreate(BaseModel):
+    title : str = Field(None, min_length=1)
+    author : str = Field(None, min_length=1)
+
+# Update Book
+class BookUpdate(BaseModel):
+    title : Optional[str] = Field(None, min_length=1)
+    author : Optional[str] = Field(None, min_length=1)
+    genre : Optional[str]
+    year : Optional[int]
+    isbn : Optional[str]
+    type : Optional[str]
+    status : Status
+    description : Optional[str]
+
+# Full Book model
+class Book(BaseModel):
+    title : str
+    author : str
+    genre : Optional[str]
+    year : Optional[int]
+    isbn : Optional[str]
+    type : Optional[str]
+    status : Status
+    description : Optional[str]
+
+    @field_validator('isbn')
+    def validate_isbn(num):
+        digits = num.replace('-','')
+        if len(digits) not in (10,13):
+            raise ValueError('ISBN code must have 10 or 13 digits.')
+        if not digits.isdigit():
+            raise ValueError('ISBN code must contain only digits.')
+        return num
