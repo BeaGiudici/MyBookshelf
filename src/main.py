5d9fa43ee5580e25308c98ff27e400a3184ff547
BeaGiudici <beatrice.giudici@gemmo.ai>
2026-03-12 14:44:05 +0100

    Adding genre endpoints to main.py

--- src/main.py
+++ src/main.py
@@ -24,6 +24,13 @@ from src.endpoints.status.read.read_status import router as read_status_router
 from src.endpoints.status.update.update_status import router as update_status_router
 from src.endpoints.status.delete.delete_status import router as delete_status_router
 
+# Genre endpoints
+from src.endpoints.genre.create.add_genre import router as add_genre_router
+from src.endpoints.genre.read.read_all import router as read_all_genres_router
+from src.endpoints.genre.read.read_genre import router as read_genre_router
+from src.endpoints.genre.update.update_genre import router as update_genre_router
+from src.endpoints.genre.delete.delete_genre import router as delete_genre_router
+
 app = FastAPI(name="My Bookshelf", version="0.1.0")
 
 # Include the necessary routers
@@ -47,6 +54,13 @@ app.include_router(read_all_statuses_router)
 app.include_router(read_status_router)
 app.include_router(update_status_router)
 app.include_router(delete_status_router)
+
+# Genre endpoints
+app.include_router(add_genre_router)
+app.include_router(read_all_genres_router)
+app.include_router(read_genre_router)
+app.include_router(update_genre_router)
+app.include_router(delete_genre_router)
 
 @app.get("/")
 def read_root():
