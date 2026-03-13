9d14808fd37596a30e4eda6bedef0b223dc3118d
BeaGiudici <beatrice.giudici@gemmo.ai>
2026-03-13 10:41:51 +0100

    Small fix

--- src/api.py
+++ src/api.py
@@ -7,6 +7,6 @@ from src.endpoints.status.router import router as status_router
 app = FastAPI(name="My Bookshelf", version="0.1.0")
 
 app.include_router(author_router)
-∫app.include_router(book_router)
+app.include_router(book_router)
 app.include_router(genre_router)
 app.include_router(status_router)
