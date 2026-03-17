6435eeb2325e4dfeec4f87c642a27cc82bbd4fe0
BeaGiudici <beatrice.giudici@gemmo.ai>
2026-03-17 10:03:39 +0100

    Fixing deletion endpoint

--- src/endpoints/author/delete_author.py
+++ src/endpoints/author/delete_author.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Depends, Body
+from fastapi import APIRouter, HTTPException, Depends, Query
 from src.endpoints.author.response import error_responses
 from src.services.authors_service import delete_author_service
 from src.database.connection import get_session
@@ -9,6 +9,6 @@ tags = ["author"]
 router = APIRouter()
 
 @router.delete(path=path, response_model=dict, responses=error_responses, tags=tags)
-async def delete_author(session: Session = Depends(get_session), author_id: int = Body(...)):
+async def delete_author(session: Session = Depends(get_session), author_id: int = Query(...)):
     msg = delete_author_service(session, author_id)
     return dict(msg=msg)
